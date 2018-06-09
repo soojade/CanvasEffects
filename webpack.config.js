acd52e14f0e90432995f307fc706d706b8a65dde
soojade <soojade@sina.com>
2018-06-09 17:43:56 +0800

    webpack 配置

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+// const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bubble: './src/bubble/index.js',
+    geometry: './src/geometry/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'www'),
+    filename: '[name]/[name].buddle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['www']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'bubble/index.html',
+      title: 'bubble effect',
+      chunks: ['bubble'],
+      inlineSource: '.(js|css)$' // 行内模式插入 js css
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'geometry/index.html',
+      title: 'geometry effect',
+      chunks: ['geometry'],
+      inlineSource: '.(js|css)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+    // new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'www'),
+    inline: true
+  }
+}
